b49325e3c8d9c1c998edab34f9735b7fcdb54130
clydeharoldloliveros-a19y <clydeharold.l.oliveros@isu.edu.ph>
2026-09-10 07:44:37 +0800

    Activity 2.6 MessageValue

---
+++ MessageValue.java
@@ -0,0 +1,10 @@
+public class MessageValue {
+    public static void main(String[] args) {
+        String Message1 = "The value of pie is:";
+        double Value = 3.1415;
+        // Display the output
+        
+        System.out.println(Message1);
+        System.out.println(Value);
+    }
+}
