2410d536a18a56945cdd3e717124b17dd429b16d
clydeharoldloliveros-a19y <clydeharold.l.oliveros@isu.edu.ph>
2026-09-10 07:41:01 +0800

    Activity 2.5 CreateMessage

---
+++ Message.java
@@ -0,0 +1,14 @@
+public class Message {
+    public static void main(String[] args) {
+        // Declare String variables with the exact messages
+        String Message1 = "I love my course";
+        String Message2 = "I love Java programming";
+        String Message3 = "I want to be a Java programmer";
+
+        // Display each message
+        System.out.println(Message1);
+        System.out.println(Message2);
+        System.out.println(Message3);
+    }
+}
+
