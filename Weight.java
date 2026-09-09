bf3424c8f3bf9ae0fc1067ec635dfbbb7795e783
clydeharoldloliveros-a19y <clydeharold.l.oliveros@isu.edu.ph>
2026-09-10 07:51:52 +0800

    Activity 2.8 Weight

---
+++ Weight.java
@@ -0,0 +1,13 @@
+public class Weight {
+    public static void main(String[] args) {
+        double pounds = 10.0;
+        double kilograms;
+        
+        kilograms = pounds * 0.454;
+        
+        System.out.println("Weight in pounds: " + pounds);
+        System.out.println("Weight in kilograms: " + kilograms);
+    }
+}
+
+Weight in pounds: 10.0 Weight in kilograms: 4.5 4
