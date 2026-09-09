e13b9cc61f4d5d906fac371ed5cf18470036ce1b
clydeharoldloliveros-a19y <clydeharold.l.oliveros@isu.edu.ph>
2026-09-10 07:55:54 +0800

    Activity 2.9 Room

---
+++ Room.java
@@ -0,0 +1,16 @@
+public class Room {
+    public static void main(String[] args) {
+        
+        // Declare variables for the length and width of the room
+        int length = 20;
+        int width = 15;
+        
+        //  Compute the floor space
+        int floorSpace = length * width;       
+        
+        // Display the result
+        System.out.println("The floor space is " + floorSpace + "square feet.");
+    }
+}
+
+The floor space is 300square feet.
