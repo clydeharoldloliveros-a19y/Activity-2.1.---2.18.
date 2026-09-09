c305ff4908024656dc523d3de881c71073ba5c4e
clydeharoldloliveros-a19y <clydeharold.l.oliveros@isu.edu.ph>
2026-09-10 07:48:52 +0800

    Activity 2.7 Circle

---
+++ Circle.java
@@ -0,0 +1,12 @@
+public class Circle {
+    public static void main(String[] args) {
+        // Assign integer value to radius
+        int radius = 2;
+        // Calculate area using the given formula
+        double area = radius * radius * 3.142;
+        // Display the result        
+        System.out.println("Radius of the circle: " + radius);
+        System.out.println("Area of the circle: " + area);
+    }
+}
+Radius of the circle: 2 Area of the circle: 12.56 8
