441ba58351a93c79f2ea3a9c19171927aafe963e
clydeharoldloliveros-a19y <clydeharold.l.oliveros@isu.edu.ph>
2026-09-10 07:32:53 +0800

    Activity 2.2 Javaparts

---
+++ Javaparts.java
@@ -0,0 +1,13 @@
+public class desiderata {
+    public static void main(String[] args) {
+        System.out.println("Go placidly amid the noise and haste,");
+        System.out.println("and remember what peace there may be in silence.");
+        System.out.println("As far as possible, without surrender,");
+        System.out.println("be on good terms with all persons.");
+        System.out.println("Speak your truth quietly and clearly;");
+        System.out.println("and listen to others, even to the dull and the ignorant,");
+        System.out.println("they too have their story.");
+        System.out.println("Avoid loud and aggressive persons.");        
+    }
+}
+public static void main(String] args) System.out.println(".."); "Go placidly amid the noise and haste,";{}
