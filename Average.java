35a8778c7fb4618489f63295e99aed483b45a54e
clydeharoldloliveros-a19y <clydeharold.l.oliveros@isu.edu.ph>
2026-09-10 08:14:52 +0800

    Activity 2.12 Average

---
+++ Average.java
@@ -0,0 +1,17 @@
+public class Average {
+    public static void main(String[] args) {
+        
+        int number1 = 10;
+        int number2 = 20;
+        int number3 = 45;
+        
+        int average = (number1 + number2 + number3) / 3;
+        
+        System.out.println("Number 1 = " + number1);
+        System.out.println("Number 2 = " + number2);
+        System.out.println("Number 3 = " + number3);
+        
+        System.out.println("Average = " + average);
+    }
+}
+Number 1=10 Number 2 = 20 Number3= 45 Average =25
