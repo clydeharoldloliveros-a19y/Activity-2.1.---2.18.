3efbd045e6722ba99562d85a318d375d3e7746a6
clydeharoldloliveros-a19y <clydeharold.l.oliveros@isu.edu.ph>
2026-09-10 08:11:51 +0800

    Activity 2.11 ConcatenateString

---
+++ ConcatenateString.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ConcatenateString {
+    public static void main(String[] args) {
+        
+        // Declare the variables
+        String name = "Kenneth";
+        String color = "Red";
+        String animal = "Cat";
+        String food = "Spaghetti"; 
+        
+        // Display the sentence 
+        System.out.println("I had a dream that " + name + " ate a " + color + "" + animal + " and said it tasted like " + food + "!");
+    }
+}
+
+Thad a dream that Kenneth ate a Red Cat and saidit tasted like Spaghettil
